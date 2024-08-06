b7bac34447744993c151b428bc0877951c008075
agokulgopi <gokulgopi2106@gmail.com>
2024-08-07 02:01:23 +0530

    Adding sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './public/**/*.html',
+    './src/**/*.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'loop-scroll': 'loop-scroll 30s linear infinite', // Adjusted duration for smoother scrolling
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
+      colors: {
+        'logo-blue1': "#69b7f9",
+        'logo-blue2': "#42a5f6",
+        'logo-blue3': "#093476",
+      },
+    },
   },
   plugins: [],
 }
-
